7dbae1db0fb228150cd201ed8d2c466d77a14bc5
zguo235 <zguo235@gmail.com>
2020-03-10 17:20:12 +0900

    Testing of single step

--- BayesRetro_environment.sh
+++ BayesRetro_environment.sh
@@ -1,14 +1,14 @@
 set -e
 
 # Load cuda and conda path.
-# source /etc/profile.d/modules.sh
-# module load cuda/9.2
-# export PATH="$HOME/miniconda3/bin:$PATH"
-# source $HOME/miniconda3/bin/activate
+source /etc/profile.d/modules.sh
+module load cuda/9.2
+export PATH="$HOME/miniconda3/bin:$PATH"
+source $HOME/miniconda3/bin/activate
 
 # Create a new conda environment.
 conda create -y -n BayesRetro python=3.6
-source activate python36
+source activate BayesRetro
 conda install -y rdkit -c rdkit
 conda install -y future tqdm
 conda install -y pytorch=0.4.1 torchvision cudatoolkit=9.0 -c pytorch
